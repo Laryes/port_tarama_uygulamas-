08dc0800917c9f6bdb0a1c6bf8e19a711828d7cf
Ata yavuz Aydoğan <121378647+Laryes@users.noreply.github.com>
2024-12-05 15:54:58 +0300

    Update porttarama.py

--- porttarama.py
+++ porttarama.py
@@ -1,27 +1,28 @@
-import socket 
+import socket
 from concurrent.futures import ThreadPoolExecutor
 
-def port_tarama(ip,port):
-
+def port_tarama(ip, port):
     try:
-        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(1)
-            s.connect(ip,port)
+            s.connect((ip, port)) 
             print(f"[+] Port {port} açık")
     except:
-        pass  
+        pass
+
 def main():
     print("Port Tarayıcı")
     ip = input("Hedef IP veya Alan Adı: ")
     start_port = int(input("Başlangıç portunu girin: "))
     end_port = int(input("Bitiş portunu girin: "))
 
+    # Alan adı verilirse, IP'yi çözümle
+    ip = socket.gethostbyname(ip)
+
     print(f"{ip} adresinde portlar taranıyor...")
     with ThreadPoolExecutor(max_workers=100) as executor:
-        for port in range(start_port,end_port +1):
+        for port in range(start_port, end_port + 1):
             executor.submit(port_tarama, ip, port)
 
 if __name__ == "__main__":
     main()
-
-
